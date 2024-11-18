12cc3d989ad47aefeed78f5de3179883f113f1ab
jaeeun <jaeoun0238@naver.com>
2024-11-18 21:22:39 +0900

    Update 정답 입력해보기

--- week-01-homework/2-qe-conditional-statements.js
+++ week-01-homework/2-qe-conditional-statements.js
@@ -17,11 +17,60 @@ else{
 const score = 70;
 
 if (score >= 90) {
-    
-}
+    console.log('예시 2: A 학점입니다.');
+  } else if (score >= 80) {
+    console.log('예시 2: B 학점입니다.');
+  } else if (score >= 70) {
+    console.log('예시 2: C 학점입니다.');
+  } else {
+    console.log('예시 2: D 학점입니다.');
+  }
 
 // 문제 3: 나이를 입력받아 성인인지 미성년자인지 출력하세요.
 
+const age = 21;
+
+if(age>19){
+    console.log('성인 입니다.');
+}
+else{
+    console.log('성인이 아닙니다')
+}
+
 // 문제 4: switch문을 사용하여 요일을 출력하세요.
 
+const day = 3;
+
+switch (day) {
+    case 1:
+    console.log('월요일입니다')
+}
+
 // 문제 5: 두 숫자를 비교하여 큰 수를 출력하세요.
+
+let a = 1;
+let b = 5;
+// let d = Math.max(a,b);
+
+// console.log(d);
+
+// 문제 5: 두 숫자를 비교하여 큰 수를 출력하세요.
+// ->
+if(a > b) {
+  // a가 b보다 크면 a출력
+  console.log(a);
+} else {
+  // 아니면 b 출력  
+  console.log(b);
+}
+
+
+
+if(a<b){
+  console.log(b);
+
+}
+else{
+  console.log(a);
+
+}
---
+++ week-01-homework/3-qe-loop.js
@@ -0,0 +1,40 @@
+// [반복문 연습 문제]
+
+// 문제 1: 1부터 10까지의 숫자를 출력하세요.
+// i는 1이다.
+// i 는 10까지 출력된다.
+for(let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// 문제 2: 1부터 10까지의 합을 계산하여 출력하세요.
+// i+i+i+i...
+let s = 0;
+for(let i = 1; i <= 10; i++){
+    s+=i;
+}
+// 문제 3: 구구단 3단을 출력하세요.
+// n=3
+// 9까지 반복하는 반복문
+let n = 3;
+for(let i = 1; i <= 9; i++){
+    n*i;
+    //console.log(n," x ",i ," = ", n*i);
+    console.log(`${n} x ${i} = ${n*i}`);
+}
+
+// 문제 4: 배열에 저장된 과일들을 하나씩 출력하세요.
+// var fruits = ['사과', '바나나', '포도'];
+let fruits = ['사과', '바나나', '포도'];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+
+// 문제 5: while문을 사용하여 5부터 1까지 역순으로 출력하세요.
+
+const arr= [1,2,3,4,5];
+const reverseArr = arr.reverse();
+
+console.log(reverseArr);
+
+
